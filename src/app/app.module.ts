5992598ee5695948e80c3600f82344af9f6e172e
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-06 18:36:08 +0300

    Add form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { QuoteFormComponent } from './components/quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
