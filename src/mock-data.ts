96431b3aa5ff0d422aa13a952c50e9b1c1395dab
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-06 20:03:25 +0300

    Add a mock data for the project

---
+++ src/mock-data.ts
@@ -0,0 +1,7 @@
+import { Quote } from "./app/blueprint/quote";
+
+export const quotes: Quote[] = [
+  new Quote('Sitaki ma missed call bana', 'Bazenga', 'Tim', new Date()),
+  new Quote('Quotes za umbwakni', 'Bazenga Daddy', 'Lord', new Date()),
+  new Quote('Sijui what the next one will be', 'Okwonko', 'Tim Lord', new Date()),
+];
