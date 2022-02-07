b55b0d09744597d4fb51800399013feb758df07e
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-07 08:14:32 +0300

    Modify the Quote class blueprint to add a show details

--- src/app/blueprint/quote.ts
+++ src/app/blueprint/quote.ts
@@ -3,11 +3,13 @@ export class Quote {
   author: string;
   addedBy: string;
   date: Date;
+  showDetails: boolean;
 
   constructor(quote: string, author: string, addedBy: string, date: Date) {
     (this.quote = quote),
       (this.author = author),
       (this.addedBy = addedBy),
       (this.date = date);
+    this.showDetails = false;
   }
 }
