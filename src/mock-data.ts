75ba9e9b9c1de69a775b23bee94bdbc3cf357025
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-07 19:33:09 +0300

    Add custom pipe to calculate the days the quote was created

--- src/mock-data.ts
+++ src/mock-data.ts
@@ -1,7 +1,7 @@
 import { Quote } from "./app/blueprint/quote";
 
 export const quotes: Quote[] = [
-  new Quote('Sitaki ma missed call bana', 'Bazenga', 'Tim', new Date()),
+  new Quote('Sitaki ma missed call bana', 'Bazenga', 'Tim', new Date('1995-12-10')),
   new Quote('Quotes za umbwakni', 'Bazenga Daddy', 'Lord', new Date()),
   new Quote('Sijui what the next one will be', 'Okwonko', 'Tim Lord', new Date()),
 ];
