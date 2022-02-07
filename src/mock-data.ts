757d275be97b99ec596df7afa00253ac9b44ff45
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-02-07 20:29:44 +0300

    Modify mock data

--- src/mock-data.ts
+++ src/mock-data.ts
@@ -1,7 +1,22 @@
 import { Quote } from "./app/blueprint/quote";
 
 export const quotes: Quote[] = [
-  new Quote('Sitaki ma missed call bana', 'Bazenga', 'Tim', new Date('1995-12-10')),
-  new Quote('Quotes za umbwakni', 'Bazenga Daddy', 'Lord', new Date()),
-  new Quote('Sijui what the next one will be', 'Okwonko', 'Tim Lord', new Date()),
+  new Quote(
+    'Spread love everywhere you go. Let no one ever come to you without leaving happier',
+    'Mother Teresa',
+    'Tim',
+    new Date('2010-12-10')
+  ),
+  new Quote(
+    'When you reach the end of your rope, tie a knot in it and hang on',
+    'Franklin D. Roosevelt',
+    'Lord',
+    new Date('2015-10-11')
+  ),
+  new Quote(
+    'Always remember that you are absolutely unique. Just like everyone else',
+    'Margaret Mead',
+    'Tim Lord',
+    new Date('2018-10-12')
+  ),
 ];
